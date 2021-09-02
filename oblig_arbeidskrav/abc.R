d18d6482f83beed9a86ec00d7ac1bb18736d15c7
Jørn Inge Halvorsen <jorn.halvorsen@gmail.com>
2021-09-02 14:00:42 +0200

    rpwithcolor

--- oblig_arbeidskrav/abc.R
+++ oblig_arbeidskrav/abc.R
@@ -13,22 +13,18 @@ unique(crypto_df$currency)
 gensh_df <- crypto_df %>% 
 	dplyr::group_by(currency) %>% 
 	dplyr::mutate(mdate=min(date)) %>%
-	dplyr::mutate(rp=close-dpyr::lag(close)) %>%
+	dplyr::mutate(rp=(close-dplyr::lag(close))/close) %>%
 	dplyr::mutate(mean=mean(close)) %>%
 	dplyr::ungroup() 
 
-View(gensh_df)
-
-plot(x=btc_df$Date,btc_df$close)
 # Tasks
 ## Sheet 1: Individuelt
 ### 1. Finn tidsintervall
 ### 2. Finn forventet verdi
 ### 3. Finn varians og standardavvik
-### 4. Graf
-ggplot2::ggplot(crypto_df, aes(x=date,y=open)) + geom_point()
-
-crypto_df
+### 4. Avkastning i prosent
+### 5. Graf
+ggplot2::ggplot(gensh_df, aes(x=date,y=rp, color=currency)) + geom_point()
 
 ## Sheet 2: Samlet
 %>% dplyr::filter(date>=max(mdate))
