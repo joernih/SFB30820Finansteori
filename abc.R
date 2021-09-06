0018d0ba8be8a783122358e556b733870163b89f
Jørn Inge Halvorsen <jorn.halvorsen@gmail.com>
2021-09-06 20:32:19 +0200

    abcdef

--- abc.R
+++ abc.R
@@ -9,6 +9,7 @@ kovv[2]<- korr[2]*std[1]*std[3]
 kovv[3]<- korr[3]*std[2]*std[3]
 cov_mat <- matrix(c(std[1]^2,kovv[1],kovv[2],korr[1],std[2]^2,kovv[3],kovv[2],kovv[3],std[3]^2),nrow=3)
 port_returns <- (sum(w *df_eks_2_6$erp))
-port_risk <- sqrt(t(w) %*% (cov_mat %*% w))
+port_risk_v <- t(w) %*% (cov_mat %*% w)
+port_risk_s <- sqrt(t(w) %*% (cov_mat %*% w))
 port_returns
 port_risk
