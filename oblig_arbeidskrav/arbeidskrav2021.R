66f149f290220c52fe52c31bec3c50872f6cfe23
Jørn Inge Halvorsen <jorn.halvorsen@gmail.com>
2021-09-26 14:35:54 +0200

    smallbreak

--- oblig_arbeidskrav/arbeidskrav2021.R
+++ oblig_arbeidskrav/arbeidskrav2021.R
@@ -18,35 +18,41 @@ stocko_df <- list.files(path="csv",pattern = "*.csv")[c(3)] %>% purrr::map_df(~r
 names(stocko_df) <- c("typeindex","date","open","high","low","close") 
 #View(crypto_df)
 #View(stocko_df)
+msheets <- list(krypto=crypto_df,boers=stocko_df)
+openxlsx::write.xlsx(msheets,file='mothersheet.xlsx',overwrite=T)
 
-## Data transformation
-gensh_df <- bind_rows(dplyr::select(crypto_df,date,typeindex,close), dplyr::select(stocko_df,date,typeindex,close)) %>% 
+## Enkeltinvesteringer
+gensh_df <- 
+	bind_rows(dplyr::select(crypto_df,date,typeindex,close), 
+		  dplyr::select(stocko_df,date,typeindex,close)) %>% 
+        # Enkeltobjekter	
 	dplyr::group_by(typeindex) %>% 
 	dplyr::mutate(mdate=min(date)) %>%
 	dplyr::mutate(rp=(close-dplyr::lag(close))/close) %>%
+	dplyr::mutate(mean=mean(close)) %>%
 	dplyr::mutate(varp=var(rp,na.rm=T))%>%
 	dplyr::mutate(stdp=varp^(1/2)) %>%
-	dplyr::mutate(mean=mean(close)) %>%
 	dplyr::ungroup() 
 
+#?
 gensh_df_1 <- gensh_df %>% dplyr::filter(typeindex=)
 ggplot2::ggplot(gensh_df_1, aes(x=date,y=rp, color=typeindex)) + geom_point()
-
-## Sheet 2: Samlet
+## Porteføljeinvesteringer
 gensh_df_2 <- dplyr::filter(gensh_df, date>=max(mdate)) %>% 
 	dplyr::select(typeindex,date,rp) %>%
 	tidyr::pivot_wider(names_from=typeindex, values_from=rp) %>%
 	tidyr::drop_na()
+
+cor(gensh_df_2$BTC,gensh_df_2$ETH)
+cor(gensh_df_2$BTC,gensh_df_2$NSE)
+cor(gensh_df_2$BTC,gensh_df_2$XRP)
+cor(gensh_df_2$XRP,gensh_df_2$ETH)
 
 ggplot2::ggplot(gensh_df_2, aes(x=BTC,y=ETH)) + geom_point()+geom_smooth(method="lm", se=TRUE, fullrange=FALSE, level=0.95)
 ggplot2::ggplot(gensh_df_2, aes(x=BTC,y=XRP)) + geom_point()
 ggplot2::ggplot(gensh_df_2, aes(x=XRP,y=ETH)) + geom_point()
 ggplot2::ggplot(gensh_df_2, aes(x=BTC,y=NSE)) + geom_point()
 
-cor(gensh_df_2$BTC,gensh_df_2$ETH)
-cor(gensh_df_2$BTC,gensh_df_2$NSE)
-cor(gensh_df_2$BTC,gensh_df_2$XRP)
-cor(gensh_df_2$XRP,gensh_df_2$ETH)
 
 
 ### 1. Finn Kovarians
