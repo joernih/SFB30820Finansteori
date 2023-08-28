c24b3da97e6f6c4b4bdba5ce61076d1002c213d8
joernih <jorn.halvorsen@gmail.com>
2023-08-28 12:41:41 +0200

    pubeks

--- inst/forelesningsnotater/timeplan.R
+++ inst/forelesningsnotater/timeplan.R
@@ -93,17 +93,17 @@ oppgaver_nett <- c(
 	  ""
 )
 oppgaver_eks <- c(
-"Test 1",
-"Test 2",
-"Test 3",
-"Test 4",
-"Test 5",
-"Test 6",
-"Test 7",
-"Test 8",
-"Test 9",
-"Test 10",
-"Test 11"
+"Eks 2021: Oppg.1: 1",
+"Eks 2021: Oppg.2: 1,2, Eks 2022: Oppg.2: 1,2",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+""
 )
 
 
