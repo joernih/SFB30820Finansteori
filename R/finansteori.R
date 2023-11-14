229d7d339a6f8bf7cf92c5cc4d7fee8c1e42fe7d
joernih <jorn.halvorsen@gmail.com>
2023-11-14 17:13:38 +0100

    abc

--- R/finansteori.R
+++ R/finansteori.R
@@ -32,3 +32,17 @@ durasjon_df <- function(r=0.0153,r_m=0.0175,n=2,T=10,M=1000){
 }
 #' @export
 sd.p=function(x){sd(x)*sqrt((length(x)-1)/length(x))}
+
+#' @export
+kob <- function(A0=620,I=680,rf=0.01,theta=1.25,n=0.85){
+  le1 <- 1/(1+rf)
+  q <- (1+rf-n)/(theta-n)
+  k_t <- max(0,theta*A0-I)
+  k_n <- max(0,n*A0-I)
+  qm1 <- 1-q
+  kob <- le1*(q*k_t+(1-q)*k_n)
+}
+print(kob())
+
+
+
